58fd346f8dba7a7fb97ee000422177f149808477
Tin21 <tin.franic8@gmail.com>
2023-04-26 17:21:14 +0200

    Applied React BEM on Section component

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -5,10 +5,10 @@ import "./Section.scss";
 
 const Section = ({title, subtitle, children}) => {
   return (
-    <section className="section">
-        <div className="section__inner">
-          <h2 className="section__title">{title}</h2>
-          <p className="section__subtitle">
+    <section className="Section">
+        <div className="Section-Inner">
+          <h2 className="Section-Title">{title}</h2>
+          <p className="Section-Subtitle">
             {subtitle}
           </p>
           {children}
