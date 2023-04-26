87d61faa6baa7bc4512a09a60e0921068c7503a5
Tin21 <tin.franic8@gmail.com>
2023-04-26 14:25:36 +0200

    Added Section component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
+import Section from "./components/Section/Section";
 
 function App() {
 
@@ -7,6 +8,11 @@ function App() {
       <Header />
       <main>
         <Hero />
+        <Section title="Open your new possibilities" subtitle="We recommend that you choose one of the featured courses. If you
+            don't find anything for you here, search for courses in detail on
+            the courses page.">
+              Djeca
+            </Section>
       </main>
     </>
   ) 
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from "prop-types";
+import "./Section.scss";
+
+
+const Section = ({title, subtitle, children}) => {
+  return (
+    <section className="section">
+        <div className="section__inner">
+          <h2 className="section__title">{title}</h2>
+          <p className="section__subtitle">
+            {subtitle}
+          </p>
+        </div>
+        {children}
+    </section>
+  )
+}
+
+Section.PropTypes = {
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    children: PropTypes.node
+}
+
+export default Section
